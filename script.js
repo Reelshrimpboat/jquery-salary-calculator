34682e270d2abf877e35bc455daee6fcf60e86d6
Reelshrimpboat <Camneelycreative@gmail.com>
2020-05-15 22:07:13 -0500

    some basics

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery is lodaed');
+    $('#submit').on('click', employeeAdd);
+}
+
+let employeeArray = [];
+
+function EmployeeInfo( firstName, lastName, idNumber , jobTitle , annualSalary ){
+    let employee = {
+        firstName: firstName,
+        lastName: lastName,
+        idNumber: idNumber,
+        jobTitle: jobTitle,
+        annualSalary: annualSalary
+    }
+    return employee;
+}
+
+function employeeAdd(){
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let idNumber = $('#idNumber').val();
+        let jobTitle = $('#jobTitle').val();
+        let annualSalary = $('#annualSalary').val();
+
+        
+        employeeArray.push(EmployeeInfo(firstName, lastName, idNumber, jobTitle, annualSalary));
+
+        console.log(employeeArray);
+
+        $('#firstName').val('');
+        $('#lastName').val('');
+        $('#idNumber').val('');
+        $('#jobTitle').val('');
+        $('#annualSalary').val('');
 }
