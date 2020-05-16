7f8401003cc70e439e619ed6b4667fdc742fa63b
Reelshrimpboat <Camneelycreative@gmail.com>
2020-05-15 23:36:55 -0500

    got the math to work, though dumbly

--- script.js
+++ script.js
@@ -30,11 +30,42 @@ function employeeAdd(){
         
         employeeArray.push(EmployeeInfo(firstName, lastName, idNumber, jobTitle, annualSalary));
 
-        console.log(employeeArray);
+        //console.log(employeeArray);
 
         $('#firstName').val('');
         $('#lastName').val('');
         $('#idNumber').val('');
         $('#jobTitle').val('');
         $('#annualSalary').val('');
+
+        addToTable(employeeArray);
+}
+
+function addToTable(employeeArray){
+    $('.table').empty();
+    $('.table').append( `<tr class="topRow"><th>First Name</th> <th>Last Name</th> <th>Employe ID</th> <th>Job Title</th> <th>Annual Salary</th> </tr>`);
+
+    for (let index = 0; index < employeeArray.length; index++) {
+        let empOut = employeeArray[index];
+        $('.table').append(`<tr><td>${empOut.firstName}</td><td>${empOut.lastName}</td><td>${empOut.idNumber}</td><td>${empOut.jobTitle}</td><td>${empOut.annualSalary}</td></tr>`);
+        
+    }
+    calcTotal(employeeArray);
+
+}
+
+function calcTotal(employeeArray) {
+    let totalMonthly = 0;
+
+    for (let index = 0; index < employeeArray.length; index++) {
+        const add = employeeArray[index];
+        let monthlyAdd = (add.annualSalary / 12);
+        console.log(monthlyAdd);
+        totalMonthly += monthlyAdd;
+        console.log(totalMonthly);
+    }
+
+    totalMonthly = (totalMonthly/employeeArray.length);
+    console.log(totalMonthly);
+    //displayMonthly(totalMonthly);
 }
